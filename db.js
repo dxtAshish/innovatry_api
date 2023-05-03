ad505ee7342d49c36f6cb76117a2cd5deb2c1926
Rishabh <rishabhgoswami901@gmail.com>
2023-05-03 13:24:11 +0530

    database change

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const mongoose= require('mongoose');
- const mongoURI="mongodb://localhost:27017/innovatoryScrapyard"
+mongoose.set("strictQuery",false)
+ const mongoURI="mongodb+srv://ashishdxt121200:qdSP7Ile7EYzRWen@innovatry.old1v4i.mongodb.net/?retryWrites=true&w=majority"
  const connectToMongo =()=>{
     mongoose.connect(mongoURI,()=>{
         console.log("connect to mongo");
